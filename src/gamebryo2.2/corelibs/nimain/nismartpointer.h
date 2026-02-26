83d5e1b44f82af0d0edd58be862ab8bc1e2c1c06
ieee 802.11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2026-02-26 12:30:29 -0800

    NiPointer

---
+++ src/gamebryo2.2/corelibs/nimain/nismartpointer.h
@@ -0,0 +1,55 @@
+#pragma once
+
+template <typename T>
+class NiPointer {
+public:
+    NiPointer(NiPointer<T> const &);
+
+    NiPointer(T *pObject) : m_pObject(pObject) {
+        if (pObject) {
+            pObject->IncRefCount();
+        }
+    }
+
+    ~NiPointer() {
+        if (m_pObject) {
+            m_pObject->DecRefCount();
+        }
+    }
+
+    operator T *() const { return m_pObject; }
+
+    T &operator*() const { return *m_pObject; }
+
+    T *operator->() const { return m_pObject; }
+
+    NiPointer<T> &operator=(T *pObject) {
+        if (m_pObject) {
+            m_pObject->DecRefCount();
+        }
+        m_pObject = pObject;
+        if (pObject) {
+            pObject->IncRefCount();
+        }
+    }
+
+    NiPointer<T> &operator=(NiPointer<T> const &rhs) {
+        if (m_pObject) {
+            m_pObject->DecRefCount();
+        }
+        m_pObject = rhs.m_pObject;
+        if (m_pObject) {
+            m_pObject->IncRefCount();
+        }
+    }
+
+    bool operator==(NiPointer<T> const &rhs) const { return m_pObject == rhs.m_pObject; }
+
+    bool operator==(T *pPointer) const { return m_pObject == pPointer; }
+
+    bool operator!=(NiPointer<T> const &rhs) const { return !(this == rhs); }
+
+    bool operator!=(T *pObject) const { return !(this == pObject); }
+
+    T *m_pObject; // 0x0
+};
